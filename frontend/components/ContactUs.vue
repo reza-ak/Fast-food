7cf34e17f688fb13c94f5c192be29ee142237741
reza-ak <reeza.dev@gmail.com>
2023-03-21 22:40:35 +0330

    Add map with leaflet to contactUs component

--- frontend/components/ContactUs.vue
+++ frontend/components/ContactUs.vue
@@ -90,9 +90,14 @@ const errors = ref([]);
 const loading = ref(false);
 const toast = useToast();
 async function contactUs() {
-  if(formData.name == "" || formData.email == "" || formData.subject == "" || formData.text == ""){
+  if (
+    formData.name == "" ||
+    formData.email == "" ||
+    formData.subject == "" ||
+    formData.text == ""
+  ) {
     toast.error("لطفا همه اطلاعات خواسته شده را وارد کنید.");
-    return
+    return;
   }
   try {
     loading.value = true;
@@ -108,4 +113,35 @@ async function contactUs() {
     loading.value = false;
   }
 }
+
+// map
+const { $leaflet } = useNuxtApp();
+onMounted(() => {
+  let map = $leaflet.map("map").setView([35.700105, 51.400394], 14);
+let tiles = $leaflet
+  .tileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw",
+    {
+      maxZoom: 18,
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      id: "mapbox/streets-v11",
+      tileSize: 512,
+      zoomOffset: -1,
+    }
+  )
+  .addTo(map);
+let marker = $leaflet
+  .marker([35.700105, 51.400394], {
+    icon: $leaflet.icon({
+      popupAnchor: [12, 6],
+      iconUrl: 'images/map/marker-icon.png',
+      ShadowUrl: 'images/map/marker-shadow.png',
+    })
+  })
+    .addTo(map)
+    .bindPopup("<b>Hello world!</b><br />I am a popup")
+    .openPopup();
+})
 </script>
