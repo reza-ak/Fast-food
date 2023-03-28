1cde3ba5506210c1c9bdece7e521f8d807de7745
reza-ak <reeza.dev@gmail.com>
2023-03-28 21:11:41 +0430

    POST request to OTP service and get login token

--- frontend/pages/auth/login.vue
+++ frontend/pages/auth/login.vue
@@ -6,6 +6,13 @@
         <div class="col-md-4 offset-md-4">
           <div class="card">
             <div class="card-body">
+              <div v-if="errors.length > 0" class="alert alert-danger">
+                <ul class="mb-0">
+                  <li v-for="(error, index) in errors" :key="index">
+                    {{ error }}
+                  </li>
+                </ul>
+              </div>
               <div class="form_container">
                 <form @submit.prevent="login()">
                   <div class="mb-3">
@@ -19,8 +26,12 @@
                       id="cellphone"
                     />
                   </div>
-                  <button type="submit" class="btn btn-primary btn-auth">
+                  <button type="submit" class="btn btn-primary btn-auth" :disabled="loading">
                     ورود
+                    <div
+                      v-if="loading"
+                      class="spinner-border spinner-border-sm ms-2"
+                    ></div>
                   </button>
                 </form>
               </div>
@@ -35,17 +46,31 @@
 
 <script setup>
 import { useToast } from "vue-toastification";
-const toast = useToast()
+const toast = useToast();
 const phoneNumber = ref(null);
-function login() {
+const errors = ref([]);
+const loading = ref(false)
+
+async function login() {
   if (phoneNumber.value == null) {
-    toast.error("شماره موبایل الزامی است.")
-    return
+    toast.error("شماره موبایل الزامی است.");
+    return;
   }
-  const pattern = /^(\+98|0)?9\d{9}$/
-  if(!pattern.test(phoneNumber.value)){
-    toast.error("فرمت شماره موبایل معتبر نیست.")
-    return
+  const pattern = /^(\+98|0)?9\d{9}$/;
+  if (!pattern.test(phoneNumber.value)) {
+    toast.error("فرمت شماره موبایل معتبر نیست.");
+    return;
+  }
+  try {
+    loading.value = true
+    const data = await $fetch("/api/auth/login", {
+      method: "POST",
+      body: { cellphone: phoneNumber.value },
+    });
+  } catch (error) {
+    errors.value = Object.values(error.data.data.message).flat();
+  } finally {
+    loading.value = false
   }
 }
 </script>
