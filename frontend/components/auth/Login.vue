d2df1da0fc420b419c223746804a7e3e276546b0
reza-ak <reeza.dev@gmail.com>
2023-03-28 21:18:07 +0430

    Putting the login form inside the vue component

---
+++ frontend/components/auth/Login.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="card-body">
+    <div v-if="errors.length > 0" class="alert alert-danger">
+      <ul class="mb-0">
+        <li v-for="(error, index) in errors" :key="index">
+          {{ error }}
+        </li>
+      </ul>
+    </div>
+    <div class="form_container">
+      <form @submit.prevent="login()">
+        <div class="mb-3">
+          <label for="cellphone" class="form-label">شماره موبایل</label>
+          <input
+            v-model="phoneNumber"
+            type="text"
+            class="form-control"
+            id="cellphone"
+          />
+        </div>
+        <button
+          type="submit"
+          class="btn btn-primary btn-auth"
+          :disabled="loading"
+        >
+          ورود
+          <div
+            v-if="loading"
+            class="spinner-border spinner-border-sm ms-2"
+          ></div>
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useToast } from "vue-toastification";
+const toast = useToast();
+const phoneNumber = ref(null);
+const errors = ref([]);
+const loading = ref(false);
+
+async function login() {
+  if (phoneNumber.value == null) {
+    toast.error("شماره موبایل الزامی است.");
+    return;
+  }
+  const pattern = /^(\+98|0)?9\d{9}$/;
+  if (!pattern.test(phoneNumber.value)) {
+    toast.error("فرمت شماره موبایل معتبر نیست.");
+    return;
+  }
+  try {
+    loading.value = true;
+    const data = await $fetch("/api/auth/login", {
+      method: "POST",
+      body: { cellphone: phoneNumber.value },
+    });
+  } catch (error) {
+    errors.value = Object.values(error.data.data.message).flat();
+  } finally {
+    loading.value = false;
+  }
+}
+</script>
--- frontend/pages/auth/login.vue
+++ frontend/pages/auth/login.vue
@@ -5,37 +5,7 @@
       <div class="row mt-5">
         <div class="col-md-4 offset-md-4">
           <div class="card">
-            <div class="card-body">
-              <div v-if="errors.length > 0" class="alert alert-danger">
-                <ul class="mb-0">
-                  <li v-for="(error, index) in errors" :key="index">
-                    {{ error }}
-                  </li>
-                </ul>
-              </div>
-              <div class="form_container">
-                <form @submit.prevent="login()">
-                  <div class="mb-3">
-                    <label for="cellphone" class="form-label"
-                      >شماره موبایل</label
-                    >
-                    <input
-                      v-model="phoneNumber"
-                      type="text"
-                      class="form-control"
-                      id="cellphone"
-                    />
-                  </div>
-                  <button type="submit" class="btn btn-primary btn-auth" :disabled="loading">
-                    ورود
-                    <div
-                      v-if="loading"
-                      class="spinner-border spinner-border-sm ms-2"
-                    ></div>
-                  </button>
-                </form>
-              </div>
-            </div>
+            <AuthLogin  />
           </div>
         </div>
       </div>
@@ -44,33 +14,3 @@
   <!-- end login section -->
 </template>
 
-<script setup>
-import { useToast } from "vue-toastification";
-const toast = useToast();
-const phoneNumber = ref(null);
-const errors = ref([]);
-const loading = ref(false)
-
-async function login() {
-  if (phoneNumber.value == null) {
-    toast.error("شماره موبایل الزامی است.");
-    return;
-  }
-  const pattern = /^(\+98|0)?9\d{9}$/;
-  if (!pattern.test(phoneNumber.value)) {
-    toast.error("فرمت شماره موبایل معتبر نیست.");
-    return;
-  }
-  try {
-    loading.value = true
-    const data = await $fetch("/api/auth/login", {
-      method: "POST",
-      body: { cellphone: phoneNumber.value },
-    });
-  } catch (error) {
-    errors.value = Object.values(error.data.data.message).flat();
-  } finally {
-    loading.value = false
-  }
-}
-</script>
