341734119d68dc5038618a231c1ba2b7a9f4774a
reza-ak <reeza.dev@gmail.com>
2023-03-30 23:30:45 +0430

    Designing profile layout and profile index page

--- frontend/pages/profile.vue
+++ frontend/pages/profile.vue
@@ -1,9 +1,11 @@
 <template>
-  profile
+  <NuxtLayout name="profile">
+    <NuxtPage /> <!-- صفحاتی که داخل پوشه به اسم همین فایل وجود دارند را میتوان با این دستور نشان داد -->
+  </NuxtLayout>
 </template>
 
 <script setup>
-  definePageMeta({
-    middleware: 'auth'
-  })
+definePageMeta({
+  middleware: "auth",
+});
 </script>
