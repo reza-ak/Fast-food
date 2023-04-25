f65919f9c4a8705fa5d6de0301d2514e640604db
reza-ak <reeza.dev@gmail.com>
2023-04-26 00:09:54 +0430

    Closing the menu after changing the route

--- frontend/components/layout/Header.vue
+++ frontend/components/layout/Header.vue
@@ -5,9 +5,8 @@
       <header class="header_section">
         <div class="container">
           <nav class="navbar navbar-expand-lg custom_nav-container">
-            <NuxtLink class="navbar-brand" href="/">
-              <!-- <span>FoodWeb</span> -->
-              <h1 class="fw-bold h2 text-light my-0"> FoodWeb </h1>
+            <NuxtLink @click="closeMenu" class="navbar-brand" href="/">
+              <h1 class="fw-bold h2 text-light my-0">FoodWeb</h1>
             </NuxtLink>
 
             <button
@@ -22,47 +21,71 @@
               <span class="navbar-toggler-icon"></span>
             </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div
+              ref="navToggle"
+              class="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
               <ul class="navbar-nav mx-auto">
                 <li class="nav-item" :class="{ active: $route.path === '/' }">
-                  <NuxtLink to="/" class="nav-link">صفحه اصلی</NuxtLink>
+                  <NuxtLink @click="closeMenu" to="/" class="nav-link"
+                    >صفحه اصلی</NuxtLink
+                  >
                 </li>
                 <li
                   class="nav-item"
                   :class="{ active: $route.path === '/menu' }"
                 >
-                  <NuxtLink to="/menu" class="nav-link">منو</NuxtLink>
+                  <NuxtLink @click="closeMenu" to="/menu" class="nav-link"
+                    >منو</NuxtLink
+                  >
                 </li>
                 <li
                   class="nav-item"
                   :class="{ active: $route.path === '/about-us' }"
                 >
-                  <NuxtLink to="/about-us" class="nav-link">درباره ما</NuxtLink>
+                  <NuxtLink @click="closeMenu" to="/about-us" class="nav-link"
+                    >درباره ما</NuxtLink
+                  >
                 </li>
                 <li
                   class="nav-item"
                   :class="{ active: $route.path === '/contact-us' }"
                 >
-                  <NuxtLink to="/contact-us" class="nav-link"
+                  <NuxtLink @click="closeMenu" to="/contact-us" class="nav-link"
                     >تماس باما</NuxtLink
                   >
                 </li>
               </ul>
               <div class="user_option">
-                <NuxtLink class="cart_link position-relative" to="/cart">
+                <NuxtLink
+                  @click="closeMenu"
+                  class="cart_link position-relative"
+                  to="/cart"
+                >
                   <i class="bi bi-cart-fill text-white fs-5"></i>
                   <ClientOnly>
                     <span
                       class="position-absolute top-0 translate-middle badge rounded-pill"
                     >
-                      {{countCartItems}}
+                      {{ countCartItems }}
                     </span>
                   </ClientOnly>
                 </NuxtLink>
-                <NuxtLink v-if="authUser === null" to="/auth/login" class="btn-auth">
+                <NuxtLink
+                  @click="closeMenu"
+                  v-if="authUser === null"
+                  to="/auth/login"
+                  class="btn-auth"
+                >
                   ورود
                 </NuxtLink>
-                <NuxtLink v-else to="/profile" class="btn-auth">
+                <NuxtLink
+                  @click="closeMenu"
+                  v-else
+                  to="/profile"
+                  class="btn-auth"
+                >
                   پروفایل
                 </NuxtLink>
               </div>
@@ -76,10 +99,19 @@
 </template>
 
 <script setup>
-import { useCartStore } from '~~/stores/cart';
+import { useCartStore } from "~~/stores/cart";
 
 const { authUser } = useAuth();
 
 const cart = useCartStore();
-const countCartItems = computed(() => cart.count)
+const countCartItems = computed(() => cart.count);
+
+const { $bootstrap } = useNuxtApp();
+const navToggle = ref(null);
+function closeMenu() {
+  const bsCollapse = new $bootstrap.Collapse(navToggle.value, {
+    toggle: false,
+  });
+  bsCollapse.hide();
+}
 </script>
