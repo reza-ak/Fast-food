d6ee5107660a53ad193de2caa38dcc83af690924
reza-ak <reeza.dev@gmail.com>
2023-05-16 00:08:30 +0430

    Installing bootstrap V5.2.3 & bootstrap-icon & vue-toastification packages and add main.css and fonts

--- adminFrontend/nuxt.config.ts
+++ adminFrontend/nuxt.config.ts
@@ -1,4 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: ["~/assets/css/main.css"],
 
+  app: {
+    head: {
+      htmlAttrs: {
+        dir: "rtl",
+        lang: "fa",
+      },
+    },
+  },
+
+  runtimeConfig: {
+    public: {
+      apiBase: "http://localhost:8000/api/admin-panel",
+    },
+  },
+
+  build: {
+    transpile: ["vue-toastification"],
+  },
 })
