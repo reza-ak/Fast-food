a9e1cd29854c05ea2cd19510f7c1d20a3a9d3019
reza-ak <reeza.dev@gmail.com>
2023-05-21 22:06:20 +0430

    Installing amcharts package and adding chart to index page

--- adminFrontend/pages/index.vue
+++ adminFrontend/pages/index.vue
@@ -1,9 +1,22 @@
 <template>
-  <h1>index</h1>
+  <div
+    class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom"
+  >
+    <h4 class="fw-bold">داشبورد</h4>
+  </div>
+
+  <ClientOnly>
+    <Chart :data="chartData"/>
+  </ClientOnly>
 </template>
 
 <script setup>
-  definePageMeta({
-    middleware: 'auth'
-  })
+definePageMeta({
+  middleware: "auth",
+});
+
+const { data: chartData } = await useFetch("/api/global", {
+  query: { url: "/transactions/chart" },
+  headers: useRequestHeaders(["cookie"]),
+});
 </script>
